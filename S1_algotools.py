096be154b8f364a73542e29b38970fac2bfad0b9
Camille CHAULAIC <chaulaic@iut-acy.local>
2019-09-26 17:01:26 +0200

    Session 1 Table Maximum Value

--- S1_algotools.py
+++ S1_algotools.py
@@ -22,10 +22,16 @@ tab_from_list = np.array(tab_list)
 #print('tab[' + str(id) + ']=' + str(tab_from_list[id]))
 #print('tab[{index}]={val}'.format(index=id, val=tab_from_list[id]))
     
-def average_above_zero(tab):
+def average_above_zero(tab:list):
     ##
     #Function that compute the average of positive value of an array
     # @param array
+    if not (isinstance(tab, list)):
+      raise ValueError('average_above_zero, expected a list as input')
+    if len(tab) == 0:
+        raise ValueError('expected a non empty list as input')
+    if not(isinstance(tab[0], (int,float))):
+        raise ValueError('average_above_zero, expected a list of numbers')
     
     som = 0
     n = 0
@@ -33,7 +39,48 @@ def average_above_zero(tab):
         if tab[id] > 0 :
             som = som + tab[id]
             n = n + 1
-    moy = som / n
-    return moy
+    if n == 0: 
+        raise ValueError('Division by 0 !!')
+    
+    return som / n
 
-print('Moyenne = ' + str(average_above_zero(tab_from_list)))
+print('Moyenne = ' + str(average_above_zero(tab_list)))
+
+'''
+Max <- 0
+for i <- 1 to NMAX do 
+    if Tab[i] > Max then 
+        Max <- Tab[i]
+
+Display(Max)
+'''
+
+'''
+Max <- 0
+Index_Max <- 0
+for i <- 1 to NMAX do 
+    if Tab[i] > Max then 
+        Max <- Tab[i]
+        IndexMax <- i
+
+Display(Max,i)
+'''
+
+def max_value(tab:list):
+    ##
+    #Function that compute the maximum value of an array
+    # @param array
+    
+    if not(isinstance(tab, list)):
+        raise ValueError('max_value, expected a list as input')
+    
+    max = 0
+    index_max = 0
+
+    for id in range(len(tab)):
+        if tab[id] > max:
+            max = tab[id]
+            index_max = id
+    return max,index_max
+
+print('Valeur max et index : ' + str(max_value(tab_list)))
