b8225ebe2c133e244c597cebd1019cf7bb5ff240
CamilleChlc <camille.chaulaic@gmail.com>
2019-09-28 20:45:49 +0200

    Bounding box (not tested)

--- S1_algotools.py
+++ S1_algotools.py
@@ -117,3 +117,30 @@ def reverse_table(tab):
     return tab
 
 print('Tableau inversé : ' + str(reverse_table(tab_list)))
+
+''' Bounding Box'''
+
+def roi_bbox(mat):
+    minCol = mat.shape[1]
+    minRow = mat.shape[0]
+    maxCol = 0
+    maxRow = 0
+    
+    for idRow in range(mat.shape[0]):
+        for idCol in range(mat.shape[1]):
+            pixVal = mat[idRow,idCol]
+            if pixVal != 0:
+                if idRow > maxRow:
+                    maxRow = idRow
+                if idCol > maxCol:
+                    maxCol = idCol
+                if idRow < minRow:
+                    minRow = idRow
+                if idCol < minCol:
+                    minCol = minRow
+    bbox = np.array([[minCol,minRow], [maxCol,minRow], [minCol, maxRow], [maxCol,maxRow]])
+    return bbox
+
+import cv2
+img = cv2.imread('img.png',0)
+print('Bounding box : ' + str(roi_bbox(img)))
