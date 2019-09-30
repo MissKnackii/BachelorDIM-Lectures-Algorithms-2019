b102043c3deb7f1f6ba05f94ac0c9f5cb19ff7ac
Camille CHAULAIC <chaulaic@iut-acy.local>
2019-09-30 11:26:01 +0200

    Test average list expected

--- test_S2.py
+++ test_S2.py
@@ -19,7 +19,7 @@ def test_average_above_zero():
 def test_average_divide_by_zero():
     tab_empty = []
     tab_neg = [3-4,-9]
-    with pytest.raises(ZeroDivisionError):
+    with pytest.raises(ValueError):
         #S1tested.average_above_zero(tab_empty)
         S1tested.average_above_zero(tab_neg)
         
