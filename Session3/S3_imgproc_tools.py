508e0e6feeb8d65bee8127b67e17580225ee6a48
Camille CHAULAIC <chaulaic@iut-acy.local>
2019-09-30 15:48:48 +0200

    Session 3 invert images

--- Session3/S3_imgproc_tools.py
+++ Session3/S3_imgproc_tools.py
@@ -8,11 +8,18 @@ Created on Mon Sep 30 13:31:19 2019
 import cv2
 import numpy as np
 
-img_gray = cv2.imread('imgGray.png',0)
-img_bgr = cv2.imread('img.png',1)
+img = cv2.imread('/Algo/BachelorDIM-Lectures-Algorithms-2019/Session3/trump.jpg')
+cv2.imshow('input',img)
+cv2.waitKey()
 
-print("Gray levels image shape = " + str(img_gray.shape[0]))
-print("BGR image shape = " + str(img_bgr.shape))
+print('image shape: ',img.shape)
 
-cv2.imshow("Gray levels image",img_gray)
-cv2.imshow("BGR image", img_bgr)
+def invert_colors_manual(input_img):   
+    for row in range(input_img.shape[0]):
+        for col in range(input_img.shape[1]):
+            for cha in range(input_img.shape[2]):
+                img[row,col,cha] = 255-img[row,col,cha]
+    return input_img
+            
+cv2.imshow('inverted input',invert_colors_manual(img))
+cv2.waitKey()
