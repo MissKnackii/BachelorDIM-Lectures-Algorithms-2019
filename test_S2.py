053f18a6fc10f6b303dcc91f0d058bed4ada16cb
Camille CHAULAIC <chaulaic@iut-acy.local>
2019-09-30 09:40:05 +0200

    test average above zero

---
+++ test_S2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 09:11:01 2019
+
+@author: chaulaic
+"""
+import S1_algotools as S1tested
+import pytest
+
+'''def test_list_expected(tab):
+    tab_list = [1,2,3-4,6,-9]
+    print('Moyenne = ' + str(S1tested.average_above_zero(tab_list))'''
+
+def test_average_above_zero():
+    tab_list = [1,2,3-4,6,-9]
+    test, lastID=S1tested.average_above_zero(tab_list)
+    assert test == 3
+
+
+    
